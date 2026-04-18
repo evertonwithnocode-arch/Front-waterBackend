21fa6f7017c455900921a1767516c69505c83c38
everton-fabriklab <everton@fabriklabs.co>
2026-04-18 11:38:52 -0300

    first commit5

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+from app.routes import upload, query
 
 app = FastAPI()
 
 print("🔥 APP INICIANDO")
+
+# 🔥 registra os routers
+app.include_router(upload.router)
+app.include_router(query.router)
+
 
 @app.get("/")
 def root():
