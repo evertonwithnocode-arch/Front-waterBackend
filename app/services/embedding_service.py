8d7ccc4b20dc5aaea007cd0f61c817794be9c1e4
everton-fabriklab <everton@fabriklabs.co>
2026-04-22 19:13:40 -0300

    first commit9

--- app/services/embedding_service.py
+++ app/services/embedding_service.py
@@ -4,5 +4,5 @@ from app.core.config import settings
 embeddings = OpenAIEmbeddings(
     model="openai/text-embedding-3-small",
     api_key=settings.OPENROUTER_API_KEY,
-    base_url="https://openrouter.ai/api/v1"
+    openai_api_base="https://openrouter.ai",
 )
