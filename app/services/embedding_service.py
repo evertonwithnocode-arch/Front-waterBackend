439c1c6b1d46108516a5416b32b7097f1035b2a5
everton-fabriklab <everton@fabriklabs.co>
2026-04-22 19:46:01 -0300

    first commit11

--- app/services/embedding_service.py
+++ app/services/embedding_service.py
@@ -1,11 +1,11 @@
 from langchain_openai import OpenAIEmbeddings
 from app.core.config import settings
-from app.services.rag_service import OPENROUTER_API_KEY
+
 
 embeddings = OpenAIEmbeddings(
     model="openai/text-embedding-3-small",
     base_url="https://openrouter.ai/api/v1",
-    api_key=OPENROUTER_API_KEY,
+    api_key=os.getenv("OPENROUTER_API_KEY"),
     check_embedding_ctx_length=False,
     model_kwargs={"encoding_format": "float"}
 )
